4bf8c9f486bb092aeeab9bd490a278d33dc7f11e
YafiYoga <103390170+YafiYoga@users.noreply.github.com>
2022-11-09 20:06:37 +0700

    FormRS.js

---
+++ assets/js/FormRS.js
@@ -0,0 +1,34 @@
+class formRumahSakit{
+    constructor(NamaRumahSakit, Alamat, Fasilitas, Deskripsi, Website, Email, Telepon){
+        this.NamaRumahSakit = NamaRumahSakit;
+        this.Alamat = Alamat;
+        this.Fasilitas = Fasilitas;
+        this.Deskripsi = Deskripsi;
+        this.Website = Website;
+        this.Email = Email;
+        this.Telepon = Telepon;
+    }
+    addRumahSakit(){
+        console.log('', 'can add rumahSakit');
+    }
+
+    editRumahSakit(){
+        console.log('', 'can edit rumah sakit')
+    }
+
+    deleteRumahSakit(){
+        console.log('', 'can delete rumah sakit')
+    }
+
+    findRumahSakit(){
+        console.log('', 'can find rumah sakit')
+    }
+
+    getRumahSakit(){
+        console.log('', 'can get rumah sakit')
+    }
+
+    changedataRumahSakit(){
+        console.log('', 'can cange data rumah sakit');
+    }
+}
