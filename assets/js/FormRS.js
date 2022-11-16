10a7756acca77b46eefad62907302f28d678aa32
iniryan <riyanadi552@gmail.com>
2022-11-16 15:23:35 +0700

    form js by damasyaine

--- assets/js/FormRS.js
+++ assets/js/FormRS.js
@@ -1,5 +1,5 @@
 class formRumahSakit{
-    constructor(id,NamaRumahSakit, Alamat, Fasilitas, Deskripsi, Website, Email, Telepon){
+    constructor(id, NamaRumahSakit, Alamat, Fasilitas, Deskripsi, Website, Email, Telepon, Provinsi, Kota, Kecamatan, Kelurahan, KodePos){
         this.id = id;
         this.NamaRumahSakit = NamaRumahSakit;
         this.Alamat = Alamat;
@@ -8,7 +8,13 @@ class formRumahSakit{
         this.Website = Website;
         this.Email = Email;
         this.Telepon = Telepon;
+        this.Provinsi = Provinsi;
+        this.Kota = Kota;
+        this.Kecamatan = Kecamatan;
+        this.Kelurahan = Kelurahan;
+        this.KodePos = KodePos;
     }
+
     addRumahSakit(){
         console.log('can add rumahSakit');
     }
@@ -33,3 +39,28 @@ class formRumahSakit{
         console.log('can cange data rumah sakit');
     }
 }
+
+let FormRS = document.getElementById('FormRS');
+
+FormRS.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let namaRS = document.getElementById('namaRS').value;
+    let Deskripsi = document.getElementById('deskripsi').value;
+    let Website = document.getElementById('website').value;
+    let Email = document.getElementById('email').value;
+    let Telepon = document.getElementById('notelp').value;
+    let Alamat = document.getElementById('alamat').value;
+    let Provinsi = document.getElementById('provinsi').value;
+    let Kota = document.getElementById('kota').value;
+    let Kecamatan = document.getElementById('kecamatan').value;
+    let Kelurahan = document.getElementById('kelurahan').value;
+    let KodePos = document.getElementById('kodepos').value;
+    let checkedValue = null; 
+    let Fasilitas = document.querySelectorAll('input[name="fasilitas"]:checked');
+    let FasilitasValue = [];
+    Fasilitas.forEach((checkbox) => {
+        FasilitasValue.push(checkbox.value);
+    });
+    let addRS = new formRumahSakit(1, namaRS, Alamat, FasilitasValue, Deskripsi, Website, Email, Telepon, Provinsi, Kota, Kecamatan, Kelurahan, KodePos);
+    console.log(addRS);
+});
