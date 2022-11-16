6866128c7bb13bdf29284c6a993ba4bd3cde68a7
YafiYoga <103390170+YafiYoga@users.noreply.github.com>
2022-11-16 22:40:35 +0700

    Update File DataPendonor.js

--- assets/js/DataPendonor.js
+++ assets/js/DataPendonor.js
@@ -1,5 +1,6 @@
 class DataPendonor {
-    constructor(NamaLengkap, DateOfBirth, KotaKelahiran, Alamat, Phone, Email, NIK, KK, gender){
+    constructor(id,NamaLengkap, DateOfBirth, KotaKelahiran, Alamat, Phone, Email, NIK, KK, gender){
+        this.id = id;
         this.NamaLengkap = NamaLengkap;
         this.DateOfBirth = DateOfBirth;
         this.KotaKelahiran = KotaKelahiran;
@@ -18,4 +19,24 @@ class DataPendonor {
         console.log('can change data pendonor');
     }
 }
+let FormDataPendonor = document.getElementById('FormDataPendonor');
+FormDataPendonor.addEventListener('submit',(e)=>
+{
+    e.preventDefauld();
+        let.NamaLengkap = document.getElementsByName('NamaLengkap').value;
+        let.DateOfBirth = document.getElementsByName('DateOfBirth').value;
+        let.KotaKelahiran = document.getElementsByName('KotaKelahiran').value;
+        let.Alamat = document.getElementsByName('Alamat').value;
+        let.Phone = document.getElementsByName('Phone').value;
+        let.Email = document.getElementsByName('Email').value;
+        let.NIK = document.getElementsByName('NIK').value;
+        let.KK = document.getElementsByName('KK').value;
+        let gender = document.querySelectorAll('input[name="gender"]:checked');
+        let genderValue = [];
+    gender.forEach((checkbox) => {
+        genderValue.push(checkbox.value);
+    });
+    letaddDataPendonor = newFormDataPendonor(1, NamaLengkap, DateOfBirth, KotaKelahiran, Alamat, Phone, Email, NIK, KK, gender);
+    console.log(addDataPendonor)
+});
 
