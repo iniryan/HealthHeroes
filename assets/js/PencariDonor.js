4ae5a19ca2c917349a999822e968ef0ae1b44ad1
YafiYoga <103390170+YafiYoga@users.noreply.github.com>
2022-11-16 22:44:29 +0700

    Update File PencariDonor.js

--- assets/js/PencariDonor.js
+++ assets/js/PencariDonor.js
@@ -1,5 +1,6 @@
 class PencariDonor  {
-         constructor(NamaLengkap, DateOfBirth, KotaKelahiran, Alamat, Phone, Email, NIK, KK, gender,id,password,role) {
+         constructor(id,NamaLengkap, DateOfBirth, KotaKelahiran, Alamat, Phone, Email, NIK, KK, gender) {
+         this.id = id;
          this.NamaLengkap = NamaLengkap;
          this.DateOfBirth = DateOfBirth;
          this.KotaKelahiran = KotaKelahiran;
@@ -22,3 +23,23 @@ class PencariDonor  {
              console.log('can get donation');
          }
 }
+let FormPencariDonor = document.getElementById('FormPencariDonor');
+FormPencariDonor.addEventListener('submit',(e)=>
+{
+    e.preventDefauld();
+        let.NamaLengkap = document.getElementsByName('NamaLengkap').value;
+        let.DateOfBirth = document.getElementsByName('DateOfBirth').value;
+        let.KotaKelahiran = document.getElementsByName('KotaKelahiran').value;
+        let.Alamat = document.getElementsByName('Alamat').value;
+        let.Phone = document.getElementsByName('Phone').value;
+        let.Email = document.getElementsByName('Email').value;
+        let.NIK = document.getElementsByName('NIK').value;
+        let.KK = document.getElementsByName('KK').value;
+        let gender = document.querySelectorAll('input[name="gender"]:checked');
+        let genderValue = [];
+    gender.forEach((checkbox) => {
+        genderValue.push(checkbox.value);
+    });
+    letaddPencariDonor = newFormPencariDonor(1, NamaLengkap, DateOfBirth, KotaKelahiran, Alamat, Phone, Email, NIK, KK, gender);
+    console.log(addPencariDonor)
+});
