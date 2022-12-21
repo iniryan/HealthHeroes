7a7c5376caa8c8e7e494c2fa9ebc17495d1af576
damasyaine07 <damasyaine007@gmail.com>
2022-12-21 09:36:21 +0700

    oop damasya

--- page/processformRS.php
+++ page/processformRS.php
@@ -1,29 +1,38 @@
-<?php   
-    $namaRS =  $_POST['namaRS'];
-    $deskripsi =  $_POST['deskripsi'];
-    $website =  $_POST['website'];
-    $email =  $_POST['email'];
-    $notelp =  $_POST['notelp'];
-    $alamat =  $_POST['alamat'];
-    $provinsi =  $_POST['provinsi'];
-    $kota =  $_POST['kota'];
-    $kecamatan =  $_POST['kecamatan'];
-    $kelurahan =  $_POST['kelurahan'];
-    $kodepos =  $_POST['kodepos'];
-    $fasilitas =  $_POST['fasilitas'];
+    <?php  
 
-    echo $namaRS.'<br>';
-    echo $deskripsi.'<br>';
-    echo $website.'<br>';
-    echo $email.'<br>';
-    echo $notelp.'<br>';
-    echo $alamat.'<br>';
-    echo $provinsi.'<br>';
-    echo $kota.'<br>';
-    echo $kecamatan.'<br>';
-    echo $kelurahan.'<br>';
-    echo $kodepos.'<br>';
-    echo $fasilitas.'<br>';
+    require 'oopFormRS.php';
+    @$namaRS =  $_POST['namaRS'];
+    @$deskripsi =  $_POST['deskripsi'];
+    @$website =  $_POST['website'];
+    @$email =  $_POST['email'];
+    @$notelp =  $_POST['notelp'];
+    @$alamat =  $_POST['alamat'];
+    @$provinsi =  $_POST['provinsi'];
+    @$kota =  $_POST['kota'];
+    @$kecamatan =  $_POST['kecamatan'];
+    @$kelurahan =  $_POST['kelurahan'];
+    @$kodepos =  $_POST['kodepos'];
+    @$fasilitas =  $_POST['fasilitas'];
+
+    // echo $namaRS.'<br>';
+    // echo $deskripsi.'<br>';
+    // echo $website.'<br>';
+    // echo $email.'<br>';
+    // echo $notelp.'<br>';
+    // echo $alamat.'<br>';
+    // echo $provinsi.'<br>';
+    // echo $kota.'<br>';
+    // echo $kecamatan.'<br>';
+    // echo $kelurahan.'<br>';
+    // echo $kodepos.'<br>';
+    // echo $fasilitas.'<br>';
+
+    $RumahSakit = new FormRS($namaRS, $alamat, $fasilitas, $deskripsi, $website, $email, $notelp, $provinsi, $kota, $kecamatan, $kelurahan, $kodepos);
+    echo "<pre>";
+    var_dump($RumahSakit);
+    echo "</pre>";
+
+    
 
     // $formRS = $_POST;
     // var_dump($formRS);
